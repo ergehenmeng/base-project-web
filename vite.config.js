2d414da2826ff202577b2584799858c8ca126fb3
wangyanbing <wangyanbing@lsbooks.com>
2024-05-07 15:44:59 +0800

    引入element-plus

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,8 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode, command}) => {
-  console.log("开发模式: %s", mode)
-  var env = loadEnv(mode, process.cwd(), 'VITE_')
-  console.log("读取到的环境变量: %s", env)
+  const env = loadEnv(mode, process.cwd())
+  console.log("启动模式: %s 环境变量: %s", mode, env)
   return {
     plugins: [AutoImport({
       resolvers: [ElementPlusResolver()],
