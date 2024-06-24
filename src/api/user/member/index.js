4e44c06fb6bf579d2775de3713aefd719f8b62df
二哥很猛 <664956140@qq.com>
2024-06-24 23:26:13 +0800

    商户列表

--- src/api/user/member/index.js
+++ src/api/user/member/index.js
@@ -2,28 +2,56 @@ import request from '@/utils/request';
 
 export const listPageApi = (params) => {
   return request.get({
-    url: '/manage/news/config/listPage',
+    url: '/manage/member/listPage',
     params
   });
 };
 
-export const createApi = (data) => {
+export const freezeApi = (data) => {
   return request.post({
-    url: '/manage/news/config/create',
+    url: '/manage/member/freeze',
     data
   });
 };
 
-export const updateApi = (data) => {
+export const unfreezeApi = (data) => {
   return request.post({
-    url: '/manage/news/config/update',
+    url: '/manage/member/unfreeze',
     data
   });
 };
 
-export const deleteApi = (data) => {
+export const offlineApi = (data) => {
   return request.post({
-    url: '/manage/news/config/delete',
+    url: '/manage/member/offline',
+    data
+  });
+};
+
+export const exportApi = (params) => {
+  return request.download({
+    url: '/manage/member/export',
+    params
+  });
+};
+
+export const loginPageApi = (params) => {
+  return request.get({
+    url: '/manage/member/loginPage',
+    params
+  });
+};
+
+export const sendNoticeApi = (data) => {
+  return request.post({
+    url: '/manage/member/sendNotice',
+    data
+  });
+};
+
+export const sendSmsApi = (data) => {
+  return request.post({
+    url: '/manage/member/sendSms',
     data
   });
 };
--- src/views/user/member/index.vue
+++ src/views/user/member/index.vue
@@ -7,9 +7,33 @@
         </el-form-item>
         <el-form-item label="状态">
           <el-select v-model="queryParams.state" clearable>
-            <el-option label="正常" value="1" />
-            <el-option label="禁用" value="0" />
+            <el-option label="正常" :value="true" />
+            <el-option label="冻结" :value="false" />
           </el-select>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-select v-model="queryParams.sex" clearable>
+            <el-option label="未知" :value="0" />
+            <el-option label="男" :value="1" />
+            <el-option label="女" :value="2" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="访问渠道">
+          <el-select v-model="queryParams.channel" clearable style="width: 120px">
+            <el-option label="PC" value="PC" />
+            <el-option label="ANDROID" value="ANDROID" />
+            <el-option label="IOS" value="IOS" />
+            <el-option label="H5" value="H5" />
+            <el-option label="WECHAT" value="WECHAT" />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="queryParams.mobile" placeholder="邀请人手机号" clearable />
+        </el-form-item>
+        <el-form-item label="注册日期">
+          <div style="width: 220px">
+            <el-date-picker type="daterange" value-format="YYYY-MM-DD" v-model="queryParams.activityDate" style="width: 220px"></el-date-picker>
+          </div>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="search">搜索</el-button>
@@ -17,28 +41,30 @@
       </el-form>
     </div>
     <div class="content-main">
-      <el-table :data="pageData" style="width: 100%" stripe v-loading="loading" max-height="670" show-overflow-tooltip>
-        <el-table-column prop="nickName" label="昵称" width="120" />
-        <el-table-column prop="mobile" label="登录账号" width="150" />
-        <el-table-column prop="userType" label="用户类型" width="100" :formatter="formatter" />
-        <el-table-column prop="dataType" label="数据权限" width="150" :formatter="formatter" />
-        <el-table-column prop="state" label="状态" width="100" :formatter="formatter" />
-        <el-table-column prop="deptName" label="所属部门" width="150" />
-        <el-table-column prop="remark" label="备注" />
-        <el-table-column prop="createTime" label="创建时间" />
-        <el-table-column prop="updateTime" label="更新时间" />
-        <el-table-column label="操作">
-          <template #header>
-            <span style="margin-right: 5px">操作</span>
-            <CreateButton v-has-perm="'gqK0'" title="新增用户" @click="handleCreate"></CreateButton>
-          </template>
+      <el-table :data="pageData" style="width: 100%" v-loading="loading" max-height="670" show-overflow-tooltip>
+        <el-table-column prop="avatar" label="头像" width="80">
           <template #default="scope">
-            <el-button v-has-perm="'mqK0'" type="info" :icon="Document" @click="handleDetail(scope.row)" link title="详情"></el-button>
-            <el-button v-has-perm="'iqK0'" type="primary" :icon="Edit" @click="handleEdit(scope.row)" link title="编辑"></el-button>
-            <el-button v-has-perm="'qqK0'" v-show="scope.row.state === 1" type="warning" :icon="Lock" @click="handleLock(scope.row)" link title="锁定"></el-button>
-            <el-button v-has-perm="'8qK0'" v-show="scope.row.state === 0" type="success" :icon="Unlock" @click="handleUnlock(scope.row)" link title="解锁"></el-button>
-            <el-button v-has-perm="'jqK0'" type="primary" :icon="Refresh" @click="handleReset(scope.row)" link title="重置密码"></el-button>
-            <el-button v-has-perm="'2qK0'" type="danger" :icon="Delete" @click="handleDelete(scope.row)" link title="删除"></el-button>
+            <div style="display: flex; align-items: center">
+              <el-image fit="contain" :src="scope.row.avatar" style="width: 50px; height: 50px" preview-teleported hide-on-click-modal />
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="nickName" label="昵称" width="120" />
+        <el-table-column prop="mobile" label="手机号码" width="120" />
+        <el-table-column prop="email" label="电子邮箱" width="180" />
+        <el-table-column prop="state" label="状态" width="80" :formatter="formatter" />
+        <el-table-column prop="score" label="积分" width="100" />
+        <el-table-column prop="inviteCode" label="邀请码" width="100" />
+        <el-table-column prop="sex" label="性别" width="80" :formatter="formatter" />
+        <el-table-column prop="realName" label="真实姓名" width="120" />
+        <el-table-column prop="birthday" label="生日" width="100" />
+        <el-table-column prop="channel" label="注册渠道" />
+        <el-table-column prop="createTime" label="注册时间" />
+        <el-table-column label="操作" min-width="200">
+          <template #default="scope">
+            <el-button v-has-perm="'qqK0'" v-show="scope.row.state === 1" type="warning" :icon="Lock" @click="handleFreeze(scope.row)" link title="冻结"></el-button>
+            <el-button v-has-perm="'8qK0'" v-show="scope.row.state === 0" type="success" :icon="Unlock" @click="handleUnFreeze(scope.row)" link title="解冻"></el-button>
+            <el-button v-has-perm="'jqK0'" type="primary" :icon="Refresh" @click="handleLogout(scope.row)" link title="强制下线"></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -52,27 +78,27 @@
       />
     </div>
   </div>
-  <UserForm ref="formRef" @reload="getPage"></UserForm>
 </template>
 <script setup>
-import { deleteApi, listPageApi, lockApi, resetPwdApi, unlockApi } from '@/api/system/user';
+import { freezeApi, listPageApi, offlineApi, unfreezeApi } from '@/api/user/member';
 import { h, onMounted, reactive, ref } from 'vue';
-import { Delete, Document, Edit, Lock, Refresh, Unlock } from '@element-plus/icons-vue';
+import { Lock, Refresh, Unlock } from '@element-plus/icons-vue';
 import { confirmMsg, successMsg } from '@/utils/message';
-import UserForm from './UserForm.vue';
 import useUserStore from '@/store/user';
-import CreateButton from '@/components/CreateButton.vue';
 
 const loading = ref(false);
 const total = ref(0);
-const formRef = ref();
 const userStore = useUserStore();
 const selectAuth = userStore.hasAuth('XqK0');
 const queryParams = reactive({
   queryName: '',
   page: 1,
   pageSize: 10,
-  state: null
+  state: null,
+  sex: null,
+  channel: null,
+  activityDate: [],
+  mobile: null
 });
 
 const pageData = ref([]);
@@ -92,24 +118,12 @@ const getPage = async () => {
 
 const formatter = (row, column, cellValue) => {
   if (column.property === 'state') {
-    return cellValue === 1 ? h('span', { style: 'color: green;' }, '正常') : h('span', { style: 'color: #ff3d3d;' }, '锁定');
-  } else if (column.property === 'userType') {
-    if (cellValue === 1) {
-      return '系统用户';
+    return cellValue ? h('span', { style: 'color: green;' }, '正常') : h('span', { style: 'color: #ff3d3d;' }, '冻结');
+  } else if (column.property === 'sex') {
+    if (cellValue === 0) {
+      return '未知';
     }
-    return cellValue === 2 ? '商户管理员' : '商户用户';
-  } else if (column.property === 'dataType') {
-    if (cellValue === 1) {
-      return '本人数据权限';
-    } else if (cellValue === 2) {
-      return '本部门数据权限';
-    } else if (cellValue === 3) {
-      return '本部门及子部门数据权限';
-    } else if (cellValue === 4) {
-      return '所有数据权限';
-    } else {
-      return '自定义数据权限';
-    }
+    return cellValue === 1 ? '男' : '女';
   } else {
     return cellValue;
   }
@@ -124,55 +138,33 @@ onMounted(() => {
   getPage();
 });
 
-const handleDetail = (row) => {
-  formRef.value.openDialog(row);
-};
-
-const handleEdit = (row) => {
-  formRef.value.openDialog(row);
-};
-
-const handleLock = (row) => {
-  confirmMsg('确定要锁定该用户吗?', () => {
+const handleFreeze = (row) => {
+  confirmMsg('确定要冻结该用户吗?', () => {
     const data = { id: row.id };
-    lockApi(data).then(() => {
-      successMsg('用户锁定成功');
+    freezeApi(data).then(() => {
+      successMsg('用户冻结成功');
       getPage();
     });
   });
 };
 
-const handleUnlock = (row) => {
-  confirmMsg('确定要解锁该用户吗?', () => {
+const handleUnFreeze = (row) => {
+  confirmMsg('确定要解冻该用户吗?', () => {
     const data = { id: row.id };
-    unlockApi(data).then(() => {
-      successMsg('用户解锁成功');
+    unfreezeApi(data).then(() => {
+      successMsg('用户解冻成功');
       getPage();
     });
   });
 };
 
-const handleReset = (row) => {
-  confirmMsg('确定要重置该用户的密码?', () => {
+const handleLogout = (row) => {
+  confirmMsg('确定要强制下线该用户?', () => {
     const data = { id: row.id };
-    resetPwdApi(data).then(() => {
-      successMsg('密码重置成功');
+    offlineApi(data).then(() => {
+      successMsg('用户强制下线成功');
       getPage();
     });
   });
-};
-
-const handleDelete = (row) => {
-  confirmMsg('确定要删除该用户吗?', () => {
-    const data = { id: row.id };
-    deleteApi(data).then(() => {
-      successMsg('用户删除成功');
-      getPage();
-    });
-  });
-};
-
-const handleCreate = () => {
-  formRef.value.openDialog({});
 };
 </script>
