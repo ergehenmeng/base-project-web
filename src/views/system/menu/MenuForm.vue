ff4b6249f65f19714504d79e5b1432155a232697
wangyanbing <wangyanbing@lsbooks.com>
2024-08-19 15:27:54 +0800

    优化代码

--- src/views/system/menu/MenuForm.vue
+++ src/views/system/menu/MenuForm.vue
@@ -84,10 +84,10 @@ const formData = ref({
 const openDialog = (row) => {
   showDialog.value = true;
   resetForm();
+  disabledIcon.value = false;
   if (row.id) {
     dialogTitle.value = '编辑菜单';
     formData.value = { ...row };
-    disabledIcon.value = false;
   } else {
     formData.value.pid = row.pid;
     // 菜单支持三级,第二级添加的一定是按钮
