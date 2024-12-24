fdcdcaad98bb7b9df9d32beafc39ff43f4b08a8b
wangyanbing <wangyanbing@lsbooks.com>
2024-12-24 11:49:15 +0800

    优化样式

--- src/views/system/dept/DeptForm.vue
+++ src/views/system/dept/DeptForm.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-dialog :title="dialogTitle" v-model="showDialog" width="550px" draggable align-center :close-on-click-modal="false">
+  <el-dialog :title="dialogTitle" v-model="showDialog" width="450px" draggable align-center :close-on-click-modal="false">
     <el-form :model="formData" ref="formDataRef" :rules="formRules" label-position="right" label-width="auto" v-loading="loading">
       <el-form-item label="部门名称" prop="title">
-        <el-input v-model="formData.title" show-word-limit maxlength="8" />
+        <el-input v-model="formData.title" show-word-limit maxlength="8" style="width: 320px !important;"/>
       </el-form-item>
       <el-form-item label="备注" prop="remark">
-        <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 3 }" v-model="formData.remark" maxlength="100" show-word-limit />
+        <el-input type="textarea" :autosize="{ minRows: 3, maxRows: 4 }" v-model="formData.remark" maxlength="100" show-word-limit style="width: 320px !important;"/>
       </el-form-item>
     </el-form>
     <template #footer>
