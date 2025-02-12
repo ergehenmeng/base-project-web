979e67b4c7f10155cb9f31487ebd04022c007314
wangyanbing <wangyanbing@lsbooks.com>
2025-02-12 14:25:29 +0800

    微信网页js

--- src/api/common/index.js
+++ src/api/common/index.js
@@ -20,3 +20,27 @@ export const qrcodeApi = (data) => {
     data
   });
 };
+
+export const WxLogin = ({ id, appId, redirectUri, state, style, href, lang, fastLogin }) => {
+  const iframe = document.createElement('iframe');
+  let url = `https://open.weixin.qq.com/connect/qrconnect?appid=${appId}&scope=snsapi_login&redirect_uri=${redirectUri}&state=${state}&login_type=jssdk&self_redirect=true&stylelite=1`;
+  if (style) {
+    url += `&style=${style}`;
+  }
+  if (href) {
+    url += `&href=${href}`;
+  }
+  if (lang === 'en') {
+    url += '&lang=en';
+  }
+  if (fastLogin === 0) {
+    url += '&fast_login=0';
+  }
+  iframe.src = url;
+  iframe.allowTransparency = 'true';
+  iframe.width = '300px';
+  iframe.height = '400px';
+  const container = document.getElementById(id);
+  container.innerHTML = '';
+  container.appendChild(iframe);
+}
