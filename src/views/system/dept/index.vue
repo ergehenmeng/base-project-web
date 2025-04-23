400d45b353489bb975ddfb41a84fc7610898460c
wangyanbing <wangyanbing@lsbooks.com>
2025-04-23 15:03:11 +0800

    高度优化

--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -11,7 +11,7 @@
       </el-form>
     </div>
     <div class="content-main">
-      <el-table row-key="code" :data="pageData" style="width: 100%" stripe v-loading="loading" max-height="660" show-overflow-tooltip>
+      <el-table row-key="code" :data="pageData" style="width: 100%" stripe v-loading="loading" max-height="700" show-overflow-tooltip>
         <el-table-column prop="title" label="部门名称" width="150"/>
         <el-table-column prop="remark" label="备注" width="300" />
         <el-table-column prop="createTime" label="创建时间" width="200" />
