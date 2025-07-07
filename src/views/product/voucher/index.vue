4940a2666ab8c157ea57ccb2009b37772a5328c2
wangyanbing <wangyanbing@lsbooks.com>
2025-07-07 16:34:18 +0800

    订单列表

--- src/views/product/voucher/index.vue
+++ src/views/product/voucher/index.vue
@@ -177,7 +177,7 @@ const handleExcel = () => {
   exportLoading.value = true;
   exportApi(queryParams)
     .then((res) => {
-      downloadExcel(res, '餐饮券列表');
+      downloadExcel(res, '餐饮列表');
     })
     .catch((error) => {
       successMsg('导出失败', error);
