3bbecd294fa14d44566f3580a10e630cdd3c3f8e
wangyanbing <wangyanbing@lsbooks.com>
2025-07-15 17:48:59 +0800

    样式调整

--- src/views/service/evaluation/index.vue
+++ src/views/service/evaluation/index.vue
@@ -5,11 +5,10 @@
         <el-form-item label="搜索">
           <el-input v-model="queryParams.queryName" placeholder="订单编号" clearable @keyup.enter="search" maxlength="30" class="w220"/>
         </el-form-item>
-        <el-form-item label="评价状态">
-          <el-select v-model="queryParams.state" clearable class="w120">
-            <el-option label="待审核" :value="0" />
-            <el-option label="审核通过" :value="1" />
-            <el-option label="审核拒绝" :value="2" />
+        <el-form-item label="状态">
+          <el-select v-model="queryParams.state" clearable class="w100">
+            <el-option label="正常" :value="1" />
+            <el-option label="屏蔽" :value="2" />
           </el-select>
         </el-form-item>
         <el-form-item label="商品分类">
@@ -50,15 +49,15 @@
             </div>
           </template>
         </el-table-column>
-        <el-table-column prop="productTitle" label="商品名称" min-width="200" :formatter="formatter"/>
+        <el-table-column prop="productTitle" label="商品名称" min-width="180" :formatter="formatter"/>
         <el-table-column prop="productType" label="商品类型" width="80" :formatter="(row, column, cellValue) => parseProductType(cellValue)"/>
         <el-table-column prop="orderNo" label="订单编号" width="200" />
         <el-table-column prop="score" label="综合评分" width="80" />
         <el-table-column prop="storeScore" label="店铺评分" width="80" />
-        <el-table-column prop="comment" label="评论" width="150" />
+        <el-table-column prop="comment" label="评论" min-width="150" />
         <el-table-column prop="commentPic" label="评论图片" width="80" >
           <template #default="scope">
-            <div style="display: flex; align-items: center">
+            <div style="display: flex; align-items: center" v-if="scope.row.commentPic && scope.row.commentPic.length > 0">
               <el-image
                 fit="cover"
                 :src="scope.row.commentPic[0]"
@@ -68,14 +67,15 @@
                 hide-on-click-modal
               />
            </div>
+           <span v-else>无</span>
           </template>
         </el-table-column>
-        <el-table-column prop="state" label="状态" width="80" :formatter="formatter"/>
+        <el-table-column prop="state" label="状态" width="60" :formatter="formatter"/>
         <el-table-column prop="nickName" label="用户昵称" width="100" />
         <el-table-column prop="anonymity" label="是否匿名" width="80" :formatter="formatter"/>
         <el-table-column prop="auditRemark" label="审核拒绝原因" min-width="120" />
         <el-table-column prop="auditName" label="审核人" width="80" />
-        <el-table-column prop="createTime" label="评价时间" width="170" />
+        <el-table-column prop="createTime" label="评价时间" width="160" />
         <el-table-column label="操作" fixed="right" width="60">
           <template #default="scope">
             <el-button v-has-perm="'T520'" v-if="scope.row.state === 1" type="primary" :icon="Coordinate" @click="handleAudit(scope.row)" link title="审核"></el-button>
