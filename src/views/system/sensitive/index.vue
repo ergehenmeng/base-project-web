b3aba374d7059caefbb6d87945b15126e70ed6e5
wangyanbing <664956140@qq.com>
2025-10-09 17:19:58 +0800

    优化代码

--- src/views/system/sensitive/index.vue
+++ src/views/system/sensitive/index.vue
@@ -3,10 +3,10 @@
     <div class="content-top">
       <el-form :inline="true" label-width="70px">
         <el-form-item label="搜索">
-          <el-input v-model="queryParams.queryName" placeholder="待校验的字符串" clearable @keyup.enter="search" maxlength="30" />
+          <el-input v-model="queryParams.queryName" placeholder="请输入匹配的字符串" clearable @keyup.enter="search" maxlength="30" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="search">校验</el-button>
+          <el-button type="primary" @click="search">匹配</el-button>
         </el-form-item>
       </el-form>
     </div>
