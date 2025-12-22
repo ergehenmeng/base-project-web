a36219c06d318f0dd02014044d0b8f0488929cf9
wangyanbing <664956140@qq.com>
2025-12-22 15:20:00 +0800

    优化代码

--- src/views/system/family/index.vue
+++ src/views/system/family/index.vue
@@ -142,7 +142,7 @@ const getList = async () => {
 
 const createHandle = (node) => {
   if (node.state) {
-    errorMsg('已绝户不支持新增');
+    errorMsg('绝户/早亡不支持新增');
     return;
   }
   formRef.value.openDialog(node, true);
