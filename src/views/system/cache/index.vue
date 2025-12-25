56c714b16724c1fe7abab1da51f7ac959e49f256
wangyanbing <664956140@qq.com>
2025-12-25 17:24:10 +0800

    缓存清理

--- src/views/system/cache/index.vue
+++ src/views/system/cache/index.vue
@@ -3,12 +3,15 @@
     <div class="content-top">
       <el-form :inline="true" label-width="70px">
         <el-form-item v-has-perm="'w7K0'" style="margin-left: 30px">
-          <el-button type="primary" :icon="Refresh" @click="handeClear" :disabled="selected.length === 0">清除缓存 </el-button>
+          <el-button type="primary" :icon="Refresh" @click="handeClear" :disabled="selected.length === 0">清除缓存</el-button>
+        </el-form-item>
+        <el-form-item v-has-perm="'w7K0'" style="float: right; padding-right: 15px">
+          <el-button type="primary" :icon="Aim" @click="deleteHandle">删除</el-button>
         </el-form-item>
       </el-form>
     </div>
     <div class="content-main">
-      <el-table :row-class-name="tableRowClass" :data="pageData"  @selection-change="handleSelected" style="width: 100%" v-loading="loading" max-height="670" show-overflow-tooltip>
+      <el-table :row-class-name="tableRowClass" :data="pageData" @selection-change="handleSelected" style="width: 100%" v-loading="loading" max-height="670" show-overflow-tooltip>
         <el-table-column type="selection" width="50" />
         <el-table-column prop="cacheName" label="缓存名称" />
         <el-table-column prop="updateTime" label="最近一次更新时间" />
@@ -16,14 +19,17 @@
       </el-table>
     </div>
   </div>
+  <CacheForm ref="formRef"/>
 </template>
 <script setup>
 import { clearApi, listPageApi } from '@/api/system/cache';
-import { Refresh } from '@element-plus/icons-vue';
+import { Aim, Refresh } from '@element-plus/icons-vue';
 import usePermStore from '@/store/perm';
 import dayjs from 'dayjs';
 import { successMsg } from '@/utils/message.js';
+import CacheForm from '@/views/system/cache/CacheForm.vue';
 
+const formRef = ref();
 const permStore = usePermStore();
 const selectAuth = permStore.hasAuth('E7K0');
 const selected = ref([]);
@@ -71,4 +77,9 @@ const tableRowClass = (row) => {
   }
   return '';
 };
+
+const deleteHandle = () => {
+  formRef.value.openDialog();
+};
+
 </script>
