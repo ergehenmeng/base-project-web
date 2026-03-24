7d5575769280da2403ec9db22ba3308d66d079ca
wangyanbing <664956140@qq.com>
2026-03-24 17:34:28 +0800

    优化代码

--- src/views/product/voucher/VoucherForm.vue
+++ src/views/product/voucher/VoucherForm.vue
@@ -9,10 +9,12 @@
         <RestaurantSelect v-model="formData.restaurantId" :disabled="disabled" :clearable="false"></RestaurantSelect>
       </el-form-item>
       <el-form-item label="分类标签" prop="tagId">
-        <VoucherTag v-model="formData.tagId" :restaurant-id="formData.restaurantId"/><QuestionTip content="标签是左侧导航分类"/>
+        <VoucherTag v-model="formData.tagId" :restaurant-id="formData.restaurantId" />
+        <QuestionTip content="标签是左侧导航分类" />
       </el-form-item>
       <el-form-item label="子标签" prop="subTag">
-        <el-input-tag v-model="formData.subTag" trigger="Space" placeholder="按空格键生成标签" :max="3" maxlength="8"/><QuestionTip content="该标签可以标注辣度、味道、重量、成分等"/>
+        <el-input-tag v-model="formData.subTag" trigger="Space" placeholder="按空格键生成标签" :max="3" maxlength="8" />
+        <QuestionTip content="该标签可以标注辣度、味道、重量、成分等" />
       </el-form-item>
       <el-form-item label="使用范围" prop="useScope">
         <el-radio-group v-model="formData.useScope">
@@ -20,46 +22,81 @@
           <el-radio :value="1">线上点餐</el-radio>
           <el-radio :value="2">扫码点餐</el-radio>
         </el-radio-group>
-        <QuestionTip content="线上点餐：线上下单后到店进行核销。扫码点餐：线下店铺直接扫码点餐"/>
+        <QuestionTip content="线上点餐：线上下单后到店进行核销。扫码点餐：线下店铺直接扫码点餐" />
+      </el-form-item>
+      <el-form-item label="划线价">
+        <el-input v-model="formData.linePrice" placeholder="小于销售价时不显示" show-word-limit maxlength="8" @keyup="formData.linePrice = numberValidator(formData.linePrice)" />
+      </el-form-item>
+      <el-form-item label="销售价" prop="salePrice">
+        <el-input v-model="formData.salePrice" show-word-limit maxlength="8" @keyup="formData.salePrice = numberValidator(formData.salePrice)" />
+      </el-form-item>
+      <el-form-item label="商品规格">
+        <div v-if="enableSpec" class="voucher-group">
+          <div class="voucher-spec">
+            <div class="voucher-spec-content" v-for="(item, index) in formData.specList" :key="index">
+              <el-form-item label="规格名称" :prop="`specList[${index}].specName`" :rules="{ required: true, message: '请输入规格名称', trigger: 'blur' }">
+                <el-input class="w140" v-model.trim="item.specName" @keyup="handleBlurSpec(index)" maxlength="8" />
+              </el-form-item>
+              <el-form-item label="规格值" :prop="`specList[${index}].valueList`" :rules="{ required: true, message: '请输入规格值', trigger: 'blur' }">
+                <div class="spec-value">
+                  <div class="spec-value-patch" v-for="(value, idx) in item.valueList" :key="idx">
+                    <el-form-item :prop="`specList[${index}].valueList[${idx}].name`" :rules="{ required: true, message: '请输入规格值', trigger: 'blur' }">
+                      <el-input class="w140" v-model.trim="value.name" maxlength="8" @keyup="handleBlurValue(index, idx)" />
+                    </el-form-item>
+                    <span class="close">
+                      <el-icon @click="handleCloseValue(index, idx)" v-if="!disabled">
+                        <CircleCloseFilled />
+                      </el-icon>
+                    </span>
+                  </div>
+                  <span class="spec-value-patch" v-if="!disabled && item.valueList.length < 50">
+                    <CreateButton title="添加规格值" @click="handleAddValue(index)" />
+                  </span>
+                </div>
+              </el-form-item>
+              <span class="spec-close" v-show="index > 0">
+                <el-icon @click="handleCloseSpec(index)" v-if="!disabled">
+                  <CircleCloseFilled />
+                </el-icon>
+              </span>
+            </div>
+            <div style="margin-bottom: 10px" v-if="!disabled">
+              <el-button type="primary" size="small" @click="handleAddSpec" v-show="formData.specList.length < 3">添加规格</el-button>
+              <el-button size="small" @click="handleDisableSpec">取消规格</el-button>
+            </div>
+          </div>
+        </div>
+        <div v-else class="voucher-spec-empty">
+          <el-button v-if="!disabled" type="primary" size="small" @click="handleEnableSpec">添加规格</el-button>
+          <span v-else>未配置规格</span>
+        </div>
       </el-form-item>
       <el-form-item label="有效日期" prop="validType">
         <el-radio-group v-model="formData.validType" @change="validTypeChange">
           <el-radio :value="0">按购买日计算</el-radio>
           <el-radio :value="1">按时间段计算</el-radio>
         </el-radio-group>
-        <QuestionTip content="注意：过期后未使用的餐饮商品会自动退款"/>
+        <QuestionTip content="注意：过期后未使用的餐饮商品会自动退款" />
       </el-form-item>
       <el-form-item label="有效期" :prop="validTypeProp" :rules="validTypeRules">
         <div v-if="formData.validType === 0">
           购买起始日起
-          <el-input style="width: 50px; margin: 0 5px" onkeyup="this.value=this.value.replace(/\D/g,'')" v-model="formData.validDays" maxlength="3"></el-input>
+          <el-input style="width: 50px; margin: 0 5px" onkeyup="this.value=this.value.replace(/\\D/g,'')" v-model="formData.validDays" maxlength="3"></el-input>
           天有效
         </div>
         <div v-else>
           <el-date-picker type="daterange" :disabled-date="disableBeforeDate" value-format="YYYY-MM-DD" v-model="formData.activityDate" style="width: 350px"></el-date-picker>
         </div>
       </el-form-item>
-      <el-form-item label="划线价" prop="linePrice">
-        <el-input v-model="formData.linePrice" placeholder="划线价小于销售价时不显示" show-word-limit maxlength="8" @keyup="formData.linePrice = numberValidator(formData.linePrice)" />
-      </el-form-item>
-      <el-form-item label="销售价" prop="salePrice">
-        <el-input v-model="formData.salePrice" show-word-limit maxlength="8" @keyup="formData.salePrice = numberValidator(formData.salePrice)" />
-      </el-form-item>
-      <el-form-item label="库存" prop="stock">
-        <el-input v-model="formData.stock" onkeyup="this.value=this.value.replace(/\D/g,'')" show-word-limit maxlength="4" />
-      </el-form-item>
-      <el-form-item label="虚拟销量" prop="virtualNum">
-        <el-input v-model="formData.virtualNum" onkeyup="this.value=this.value.replace(/\D/g,'')" show-word-limit maxlength="3" />
-      </el-form-item>
       <el-form-item label="限购数量" prop="quota">
-        <el-input v-model="formData.quota" onkeyup="this.value=this.value.replace(/\D/g,'')" show-word-limit maxlength="3" />
+        <el-input v-model="formData.quota" onkeyup="this.value=this.value.replace(/\\D/g,'')" show-word-limit maxlength="3" />
       </el-form-item>
       <el-form-item label="封面图" prop="coverUrl">
         <UploadImageList v-model:file-list="formData.coverUrl" :disabled="disabled"></UploadImageList>
       </el-form-item>
       <el-form-item label="商品介绍" prop="introduce">
-        <WangEditor v-if="!disabled" v-model:html-value="formData.introduce" ></WangEditor>
-        <div v-else v-html="formData.introduce" class="html-preview"/>
+        <WangEditor v-if="!disabled" v-model:html-value="formData.introduce"></WangEditor>
+        <div v-else v-html="formData.introduce" class="html-preview" />
       </el-form-item>
     </el-form>
     <div>
@@ -73,16 +110,19 @@
     </div>
   </div>
 </template>
+
 <script setup>
 import { createApi, selectApi, updateApi } from '@/api/product/voucher';
 import WangEditor from '@/components/WangEditor.vue';
 import { useRoute, useRouter } from 'vue-router';
-import { successMsg } from '@/utils/message.js';
+import { errorMsg, successMsg } from '@/utils/message.js';
 import RestaurantSelect from '@/components/RestaurantSelect.vue';
 import { disableBeforeDate, goBack, numberValidator } from '@/utils/common.js';
 import UploadImageList from '@/components/UploadImageList.vue';
-import VoucherTag from '@/components/VoucherTag.vue'
-import QuestionTip from '@/components/QuestionTip.vue'
+import VoucherTag from '@/components/VoucherTag.vue';
+import QuestionTip from '@/components/QuestionTip.vue';
+import { CircleCloseFilled } from '@element-plus/icons-vue';
+import CreateButton from '@/components/CreateButton.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -91,78 +131,176 @@ const formDataRef = ref();
 const disabled = ref(false);
 const validTypeProp = ref('validDays');
 const validTypeRules = ref({});
+const enableSpec = ref(false);
 
 const formRules = reactive({
   title: [{ required: true, message: '商品名称不能为空', trigger: 'blur' }],
   restaurantId: [{ required: true, message: '请选择所属店铺', trigger: 'change' }],
   tagId: [{ required: true, message: '请选择标签', trigger: 'change' }],
+  salePrice: [{ required: true, message: '销售价不能为空', trigger: 'blur' }],
   validType: [{ required: true, message: '请选择有效期', trigger: 'change' }],
   useScope: [{ required: true, message: '请选择使用范围', trigger: 'change' }],
-  salePrice: [{ required: true, message: '销售价不能为空', trigger: 'blur' }],
-  timeList: [{ required: true, message: '可用时间段不能为空', trigger: 'blur', type: 'array' }],
-  stock: [{ required: true, message: '库存不能为空', trigger: 'blur' }],
   coverUrl: [{ required: true, message: '封面图不能为空', trigger: 'change', type: 'array' }],
   quota: [{ required: true, message: '限购数量不能为空', trigger: 'blur' }],
   introduce: [{ required: true, message: '商品介绍不能为空', trigger: 'change' }]
 });
 
-let formData = ref({
+const formData = ref({
   id: null,
   title: null,
   restaurantId: null,
   tagId: null,
   linePrice: null,
   salePrice: null,
-  stock: null,
-  validType: 0,
-  virtualNum: null,
   quota: 99,
+  validType: 0,
   validDays: null,
   effectDate: null,
   expireDate: null,
-  timeList: [],
-  effectTime: null,
-  expireTime: null,
   useScope: 0,
   subTag: [],
   coverUrl: [],
   activityDate: [],
-  introduce: null
+  introduce: null,
+  specList: []
 });
 
-const handleSave = () => {
-  formDataRef.value.validate((valid) => {
-    if (valid) {
-      loading.value = true;
-      if (formData.value.validType === 1) {
-        formData.value.effectDate = formData.value.activityDate[0];
-        formData.value.expireDate = formData.value.activityDate[1];
-      } else {
-        formData.value.effectDate = null;
-        formData.value.expireDate = null;
-      }
-      formData.value.effectTime = formData.value.timeList[0];
-      formData.value.expireTime = formData.value.timeList[1];
-      if (formData.value.id) {
-        updateApi(formData.value)
-          .then(() => {
-            successMsg('商品信息更新成功');
-            goBack(router);
-          })
-          .finally(() => {
-            loading.value = false;
-          });
-      } else {
-        createApi(formData.value)
-          .then(() => {
-            successMsg('商品信息添加成功');
-            goBack(router);
-          })
-          .finally(() => {
-            loading.value = false;
-          });
-      }
+const buildDefaultSpecList = () => {
+  return [
+    {
+      id: null,
+      level: 1,
+      specName: null,
+      valueList: [{ name: null }]
     }
+  ];
+};
+
+const handleEnableSpec = () => {
+  enableSpec.value = true;
+  if (!formData.value.specList || formData.value.specList.length === 0) {
+    formData.value.specList = buildDefaultSpecList();
+  }
+};
+
+const handleDisableSpec = () => {
+  enableSpec.value = false;
+  formData.value.specList = [];
+};
+
+const handleAddSpec = () => {
+  if (!enableSpec.value) {
+    return;
+  }
+  if (formData.value.specList.length >= 3) {
+    errorMsg('最多支持三级规格定义');
+    return;
+  }
+  formData.value.specList.push({
+    id: null,
+    level: formData.value.specList.length + 1,
+    specName: null,
+    valueList: [{ name: null }]
+  });
+};
+
+const handleCloseSpec = (index) => {
+  if (formData.value.specList.length <= 1) {
+    errorMsg('至少保留一个规格');
+    return;
+  }
+  formData.value.specList.splice(index, 1);
+  formData.value.specList.forEach((item, idx) => {
+    item.level = idx + 1;
+  });
+};
+
+const handleAddValue = (index) => {
+  formData.value.specList[index].valueList.push({ name: null });
+};
+
+const handleCloseValue = (index, idx) => {
+  if (formData.value.specList[index].valueList.length === 1) {
+    errorMsg('至少保留一个规格值');
+    return;
+  }
+  formData.value.specList[index].valueList.splice(idx, 1);
+};
+
+const handleBlurSpec = (index) => {
+  const name = formData.value.specList[index].specName;
+  if (!name) {
+    return;
+  }
+  const repeat = formData.value.specList.filter((item, idx) => idx !== index && item.specName === name).length > 0;
+  if (repeat) {
+    errorMsg('规格名不能重复');
+    formData.value.specList[index].specName = null;
+  }
+};
+
+const handleBlurValue = (index, idx) => {
+  const spec = formData.value.specList[index];
+  const valueList = spec.valueList.filter((item) => !!item.name).map((item) => item.name);
+  if (new Set(valueList).size !== valueList.length) {
+    errorMsg('规格值不能重复');
+    spec.valueList[idx].name = null;
+  }
+};
+
+const parseSpecConfig = (rawValue) => {
+  if (!rawValue) {
+    return null;
+  }
+  if (typeof rawValue === 'object') {
+    return rawValue;
+  }
+  try {
+    return JSON.parse(rawValue);
+  } catch (_e) {
+    return null;
+  }
+};
+
+const buildSpecConfig = () => {
+  if (!enableSpec.value) {
+    return null;
+  }
+  return {
+    specList: formData.value.specList
+  };
+};
+
+const handleSave = () => {
+  if (!enableSpec.value) {
+    formData.value.specList = [];
+  }
+  formDataRef.value.validate((valid) => {
+    if (!valid) {
+      return;
+    }
+    loading.value = true;
+    if (formData.value.validType === 1) {
+      formData.value.effectDate = formData.value.activityDate[0];
+      formData.value.expireDate = formData.value.activityDate[1];
+    } else {
+      formData.value.effectDate = null;
+      formData.value.expireDate = null;
+    }
+    const specConfig = buildSpecConfig();
+    const payload = {... formData.value,
+      specEnabled: enableSpec.value,
+      specConfigJson: specConfig ? JSON.stringify(specConfig) : null
+    };
+    const api = formData.value.id ? updateApi : createApi;
+    api(payload)
+      .then(() => {
+        successMsg(formData.value.id ? '商品信息更新成功' : '商品信息添加成功');
+        goBack(router);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
   });
 };
 
@@ -170,21 +308,25 @@ onMounted(() => {
   const params = route.params;
   if (params.id !== undefined) {
     loading.value = true;
-    // 详情页面进来不可点击
     disabled.value = route.fullPath.startsWith('/product/voucher/detail');
     selectApi(params)
       .then((res) => {
-        formData.value = { ...res.data };
-        formData.value.timeList = [res.data.effectTime, res.data.expireTime];
+        formData.value = { ...formData.value, ...res.data };
         formData.value.validType = formData.value.validDays > 0 ? 0 : 1;
         validTypeChange(formData.value.validType);
         if (res.data.validDays > 0) {
-          formData.value.validType = 0;
-          formData.value.validDays = res.data.validDays;
+          formData.value.activityDate = null;
         } else {
           formData.value.validDays = null;
-          formData.value.validType = 1;
           formData.value.activityDate = [res.data.effectDate, res.data.expireDate];
+        }
+        const specConfig = parseSpecConfig(res.data.specConfigJson);
+        if (!specConfig || !specConfig.specList || specConfig.specList.length === 0) {
+          enableSpec.value = false;
+          formData.value.specList = [];
+        } else {
+          enableSpec.value = true;
+          formData.value.specList = specConfig.specList;
         }
       })
       .finally(() => {
@@ -207,3 +349,57 @@ const validTypeChange = (val) => {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.voucher-group {
+  width: 1000px;
+  border: 1px solid #dcdfe6;
+  padding: 10px;
+}
+
+.voucher-spec-empty {
+  min-height: 32px;
+  display: flex;
+  align-items: center;
+}
+
+.voucher-spec {
+  .voucher-spec-content {
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.02);
+    border-radius: 5px;
+    padding: 10px;
+    margin-bottom: 15px;
+
+    .spec-close {
+      position: absolute;
+      top: -15px;
+      right: -5px;
+      cursor: pointer;
+    }
+  }
+}
+
+.spec-value {
+  display: flex;
+  flex-wrap: wrap;
+
+  .spec-value-patch {
+    margin-right: 20px;
+    padding: 5px;
+    text-align: center;
+    position: relative;
+
+    .close {
+      position: absolute;
+      top: -15px;
+      right: -5px;
+      cursor: pointer;
+    }
+  }
+}
+
+.el-form-item .el-form-item {
+  margin-bottom: 18px;
+}
+</style>
