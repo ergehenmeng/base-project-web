6f28a8c7dd3e96fb5c25e739165f56679cabf7a8
wyb <dianxiaoer@mandofin.com>
2026-07-31 16:53:21 +0800

    优化代码

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="login-layout">
       <div class="login-layout-left"></div>
       <div class="login-layout-right">
-        <div class="login-switch-tab">
+        <div class="login-switch-tab" :class="{ 'is-sms': tabIndex === 1 }">
           <a href="javascript:void(0);" :class="{ active: tabIndex === 0 }" @click="tabIndex = 0">密码登录</a>
           <a href="javascript:void(0);" :class="{ active: tabIndex === 1 }" @click="tabIndex = 1">短信登录</a>
         </div>
@@ -59,10 +59,22 @@ const systemName = import.meta.env.VITE_SYSTEM_NAME;
       place-items: center;
       background-color: #fff;
       .login-switch-tab {
+        position: relative;
         margin-top: 10px;
         text-align: center;
-        :first-child {
-          border-right: 2px solid #e5e8ec;
+        &::after {
+          content: '';
+          position: absolute;
+          bottom: -8px;
+          left: 20px;
+          width: calc(50% - 40px);
+          height: 2px;
+          border-radius: 2px;
+          background-color: #409eff;
+          transition: transform 220ms ease-in-out;
+        }
+        &.is-sms::after {
+          transform: translateX(calc(100% + 40px));
         }
         a {
           color: #535c68;
